679522840eb91f2bb64736c2236a0343815d9056
bsjoberg <brian_sjoberg@yahoo.com>
2019-04-18 14:04:25 -0400

    Pending steps.

---
+++ Secure Password/src/test/java/fun/bdd/PasswordCheckSteps.java
@@ -0,0 +1,25 @@
+package fun.bdd;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PasswordCheckSteps {
+	@Given("I want to create an account")
+	public void iWantToCreateAnAccount() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I enter {string}")
+	public void iEnter(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the account is created")
+	public void theAccountIsCreated() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+}
--- Secure Password/src/test/resources/features/secure_password.feature
+++ Secure Password/src/test/resources/features/secure_password.feature
@@ -2,8 +2,14 @@ Feature: Secure password
 	In order to prevent hackers from compromising member accounts
 	As a privacy concerned user
 	I want new members to provide a secure password then they register
+	
+	AC:
+	The password is at least 8 characters
+	The password contains at least 1 digit
+	The password contains at least 1 punctuation mark
+	Displays error message telling user what they did wrong if an insecure password is entered
 
 Scenario: User enters secure password
 	Given I want to create an account
-	When I enter <password>
-	Then the account is created	
+	When I enter "H3lloW0rld!!!"
+	Then the account is created
