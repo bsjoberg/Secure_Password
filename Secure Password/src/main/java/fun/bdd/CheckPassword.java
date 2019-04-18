2b0018ed2da3cb64cb0c3a3f18d559d40c608255
bsjoberg <brian_sjoberg@yahoo.com>
2019-04-18 14:52:56 -0400

    Initial tests working for password checking of numbers and letters.

--- Secure Password/src/main/java/fun/bdd/CheckPassword.java
+++ Secure Password/src/main/java/fun/bdd/CheckPassword.java
@@ -3,7 +3,27 @@ package fun.bdd;
 public class CheckPassword {
 
 	public static boolean isSecure(String password) {
-		return true;
+		if (password.length() >= 8) { 
+			// Password must have characters and numbers
+			if (hasNumber(password)) {
+				return true;
+			}
+			else
+				return false;
+		}
+		else
+			return false;
 	}
 
+	private static boolean hasNumber(String password) {
+		boolean hasNumber = false;
+		
+		for (int i = 0; i <= 9; i++) {
+			if (password.contains(String.valueOf(i))) {
+				hasNumber = true;
+				break;
+			}
+		}
+		return hasNumber;
+	}
 }
--- Secure Password/src/test/java/fun/bdd/CheckPasswordTest.java
+++ Secure Password/src/test/java/fun/bdd/CheckPasswordTest.java
@@ -19,5 +19,15 @@ public class CheckPasswordTest {
 	public void test_should_be_a_secure_password() {
 		Assert.assertTrue(CheckPassword.isSecure("12345The!!"));
 	}
+	
+	@Test
+	public void should_be_insecure_with_not_enough_characters() {
+		Assert.assertFalse(CheckPassword.isSecure("1244"));
+	}
+	
+	@Test
+	public void should_be_insecure_with_no_numbers_with_8_characters() {
+		Assert.assertFalse(CheckPassword.isSecure("abcdedfg"));
+	}
 
 }
